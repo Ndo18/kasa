ed3e30930a75de152e27ba9db508d34e3387f250
Ndo18 <samuel.dmb@outlook.fr>
2024-01-16 04:11:57 +0100

    💄 Mobile version updates for header + homepage + errorpage + apropos page

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -4,13 +4,18 @@ import './stylesComponents/Collapse.css'
 function Collapse ({id, titre, description}) {
     
 const [isOpen, setisOpen] = useState(false)
+
+const iconOpen = isOpen ? 'opened' : 'closed'
     
     return (
         <div className="monBlock monBlockapropos">
-            <div className="listedescription listapropos" key={id}>
-                <h3 onClick={() => setisOpen(!isOpen)}>
+            <div onClick={() => setisOpen(!isOpen)} className={`listedescription listapropos ${iconOpen}`} key={id}>
+                <h3>
                 {titre}
                 </h3>
+                <div>
+                <i class="fa-solid fa-chevron-up"></i>
+                </div>
             </div>
         { isOpen && <div className="descriptiontext"><span>{description}</span></div> }
         </div>

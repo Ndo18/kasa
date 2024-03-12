eebf6dd8436951d2f84d440d43ebdaf252828fce
Ndo18 <samuel.dmb@outlook.fr>
2024-03-13 00:53:18 +0100

    🐛 GitHub Page bug fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,11 @@
 import { NavLink } from 'react-router-dom'
 
-
  function Header() {
         return (<div className="header">
         <nav>
-        <img src="/logokasa.png" alt='Logo Kasa' className='logokasa' />
+        <img src='logokasa.png' alt='Logo Kasa' className='logokasa' />
         <ul>
-        <li><NavLink to="/">Accueil</NavLink></li>
+        <li><NavLink to="/Accueil-Kasa">Accueil</NavLink></li>
         <li><NavLink to="/Apropos">À propos</NavLink></li>
         </ul>
         </nav>
